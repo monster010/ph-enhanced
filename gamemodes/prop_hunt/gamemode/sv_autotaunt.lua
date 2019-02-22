6727ae3be102130006aee8ea582b739030547ac5
vinzuerio <wolvindra.vinzuerio@gmail.com>
2019-02-22 13:27:15 +0700

    Autotaunt ConVar fix

--- gamemodes/prop_hunt/gamemode/sv_autotaunt.lua
+++ gamemodes/prop_hunt/gamemode/sv_autotaunt.lua
@@ -10,16 +10,21 @@ local function TauntTimeLeft(ply)
 end
 
 local function AutoTauntThink()
-	local WHOLE_TAUNTS = PHE:GetAllTeamTaunt(TEAM_PROPS)
-	for _, ply in ipairs(team.GetPlayers(TEAM_PROPS)) do
-		local timeLeft = TauntTimeLeft(ply)
-		
-		if IsValid(ply) && ply:Alive() && ply:Team() == TEAM_PROPS && timeLeft <= 0 then
-			local rand_taunt = table.Random(WHOLE_TAUNTS)
-			if !isstring(rand_taunt) then rand_taunt = tostring(rand_taunt); end
-			ply:EmitSound(rand_taunt, 100)
-			ply:SetNWFloat("LastTauntTime", CurTime())
+
+	if GetConVar("ph_autotaunt_enabled"):GetBool() then
+
+		local WHOLE_TAUNTS = PHE:GetAllTeamTaunt(TEAM_PROPS)
+		for _, ply in ipairs(team.GetPlayers(TEAM_PROPS)) do
+			local timeLeft = TauntTimeLeft(ply)
+			
+			if IsValid(ply) && ply:Alive() && ply:Team() == TEAM_PROPS && timeLeft <= 0 then
+				local rand_taunt = table.Random(WHOLE_TAUNTS)
+				if !isstring(rand_taunt) then rand_taunt = tostring(rand_taunt); end
+				ply:EmitSound(rand_taunt, 100)
+				ply:SetNWFloat("LastTauntTime", CurTime())
+			end
 		end
-    end
+		
+	end
 end
 timer.Create("AutoTauntThinkTimer", 1, 0, AutoTauntThink)
